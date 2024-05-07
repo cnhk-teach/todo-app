bbe5def037595356d29a1a12a92a7c1bb4bf9bb7
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-08 01:10:11 +0530

    Add animation and routerlink in Login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  animations:[
+    trigger('fadein',[
+      transition('void=>*',[
+        style({opacity: 0}),
+        animate('1200ms', style({opacity: 1})),
+      ])
+    ])
+  ]
 })
 export class LoginComponent {
     email = "";
@@ -15,5 +25,5 @@ export class LoginComponent {
 
     login(){
       this.displayText = this.email + this.password;
-    };
+    }
 }
