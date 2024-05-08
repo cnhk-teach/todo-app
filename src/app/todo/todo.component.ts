b90d28817a107544733246d53f19aaf7f2cb1c69
Harish Kumar Nagarajan <cnharishnagaraj@gmail.com>
2024-05-09 02:10:56 +0530

    Add todo page with neccessary elements

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
   imports: [],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrl: './todo.component.css',
+  animations:[
+    trigger('fadein',[
+      transition('void=>*',[
+        style({opacity: 0}),
+        animate('1200ms', style({opacity: 1})),
+      ])
+    ])
+  ]
 })
 export class TodoComponent {
 
+  constructor(private router: Router) {}
+
+  logout() {
+    localStorage.clear()
+    this.router.navigate(['/login'])
+  }
 }
